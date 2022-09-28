6680ccfec0165f9d185d79fc590cedaf5a84098c
Tung Le <tung.le0319@gmail.com>
2022-09-28 11:40:11 -0600

    tung

--- src/components/CarCard.vue
+++ src/components/CarCard.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="car-card col-md-3 mx-2 position-relative">
+  <div class="car-card col-md-3 mx-2 ">
  <div class="card   elevation-3 mx-4 my-2 " style="width:18rem;">
             <div class="d-flex justify-content-center">
               <img :src="car.imgUrl" alt=""
                 class="img-fluid rounded mt-2 elevation-4 forcedimg ">
             </div>
             <div class="card-body ">
+              <div>
+                <img :src="seller.picture" :alt="seller.name" :title="seller.name" height="40">
+                <i class="mdi mdi-minus fs-1"></i>
+              </div>
               <div class="d-flex justify-content-between">
                 <h6>make: {{car.make}}</h6>
                 <h6>model: {{car.model}}</h6>
               </div>
-              <div class=" mt-2">
-                <h6>year: {{car.year}}</h6>
-              </div>
-              <div>
-                <small><b>Description:   </b> {{car.description}}</small>
-              </div>
+              
+            
             </div>
           </div>
-<div class="position-absolute end-50 top-0 bg-dark rounded p-2 selectable">{{car.price}}</div>
+
   </div>
 </template>
 
@@ -45,7 +45,7 @@ seller:{type: SellerProfile, required:true}
 async deleteClassified(){
   try {
     const yes = await Pop.confirm('You sure?')
-    
+
       
     } catch (error) {
       console.error('[]',error)
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -24,6 +24,7 @@ import Pop from "../utils/Pop.js"
 export default {
 
   setup(){
+
 // async function getCars(){
 //   try {
 //       await carsService.getCars()
@@ -32,6 +33,7 @@ export default {
 //     Pop.error(error)
 //     }
 // }
+
 onMounted(()=>{
   // getCars()
 getClassifieds()
