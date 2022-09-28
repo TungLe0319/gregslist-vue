f478efecad2a05640609c166eb71d14915e3af53
Tung N Le <tung.le0319@gmail.com>
2022-09-27 21:55:30 -0600

    tung

--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -2,12 +2,16 @@ import { AppState } from "../AppState.js"
 import { House } from "../models/House.js"
 import { api } from "./AxiosService.js"
 
-class HousesService{
+class HousesService {
+  async getHouses() {
+    const res = await api.get('/api/houses');
+    AppState.houses = res.data.map((h) => new House(h));
+  }
 
-
-  async getHouses(){
-    const res = await api.get('/api/houses')
-    AppState.houses = res.data.map(h => new House(h))
+  async addHouse(editable) {
+    const res = await api.post('/api/houses');
+    // let job = new Job(res.data)
+    //AppState.jobs = [...AppState.jobs, job]
   }
 }
 
--- src/services/JobsService.js
+++ src/services/JobsService.js
@@ -8,6 +8,12 @@ async getJobs(){
   const res = await api.get('/api/jobs')
   AppState.jobs = res.data.map(j=> new Job(j))
 }
+
+async addJob(editable){
+const res = await api.post('/api/jobs')
+// let job = new Job(res.data)
+//AppState.jobs = [...AppState.jobs, job]
+}
 }
 
 export const jobsService = new JobsService()
